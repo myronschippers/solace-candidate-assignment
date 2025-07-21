e858d3d320e756b3ff0c87188bf2fca7feb67698
Myron R Schippers Jr <dubtwee@gmail.com>
2025-07-20 20:32:35 -0400

    adding typing for advocates, adding keys on mapped lists, and correcting advocate filetered search based on the the DB data types

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,22 @@
 'use client';
 
-import { useEffect, useState } from 'react';
+import { useEffect, useState, ChangeEvent } from 'react';
+
+type Advocate = {
+  city: string;
+  createdAt: string;
+  degree: string;
+  firstName: string;
+  id: number;
+  lastName: string;
+  phoneNumber: number;
+  specialties: string[];
+  yearsOfExperience: number;
+};
 
 export default function Home() {
-  const [advocates, setAdvocates] = useState([]);
-  const [filteredAdvocates, setFilteredAdvocates] = useState([]);
+  const [advocates, setAdvocates] = useState<Advocate[]>([]);
+  const [filteredAdvocates, setFilteredAdvocates] = useState<Advocate[]>([]);
 
   useEffect(() => {
     console.log('fetching advocates...');
@@ -16,7 +28,7 @@ export default function Home() {
     });
   }, []);
 
-  const onChange = (e) => {
+  const onChange = (e: ChangeEvent<HTMLInputElement>) => {
     const searchTerm = e.target.value;
 
     document.getElementById('search-term').innerHTML = searchTerm;
@@ -29,7 +41,7 @@ export default function Home() {
         advocate.city.includes(searchTerm) ||
         advocate.degree.includes(searchTerm) ||
         advocate.specialties.includes(searchTerm) ||
-        advocate.yearsOfExperience.includes(searchTerm)
+        `${advocate.yearsOfExperience}`.includes(searchTerm)
       );
     });
 
@@ -69,14 +81,14 @@ export default function Home() {
         <tbody>
           {filteredAdvocates.map((advocate) => {
             return (
-              <tr>
+              <tr key={advocate.id}>
                 <td>{advocate.firstName}</td>
                 <td>{advocate.lastName}</td>
                 <td>{advocate.city}</td>
                 <td>{advocate.degree}</td>
                 <td>
-                  {advocate.specialties.map((s) => (
-                    <div>{s}</div>
+                  {advocate.specialties.map((s, sIndex) => (
+                    <div key={sIndex}>{s}</div>
                   ))}
                 </td>
                 <td>{advocate.yearsOfExperience}</td>
