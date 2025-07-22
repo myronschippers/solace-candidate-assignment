514c2a03490c7997d39317ece3770fcbec0328f2
Myron R Schippers Jr <dubtwee@gmail.com>
2025-07-22 16:50:16 -0400

    creating a basic content wrapper for structuring the width of content

---
+++ src/components/ui/ContentWrapper/ContentWrapper.tsx
@@ -0,0 +1,34 @@
+'use client';
+
+import { useMemo, type FC } from 'react';
+
+import { DEFAULT_WIDTH, DEFAULT_VARIANT } from './constants';
+import type { ContentWrapperProps } from './types';
+
+const ContentWrapper: FC<ContentWrapperProps> = ({
+  children,
+  variant = DEFAULT_VARIANT,
+}) => {
+  const wrapperWidth = useMemo<string>(() => {
+    switch (variant) {
+      case 'narrow':
+        return '800px';
+      case 'wide':
+        return '1200px';
+      case 'full':
+        return '100%';
+      case 'regular':
+        return DEFAULT_WIDTH;
+      default:
+        return DEFAULT_WIDTH;
+    }
+  }, [variant]);
+
+  return (
+    <div className={`max-width-[100%] width-[${wrapperWidth}] mx-auto`}>
+      {children}
+    </div>
+  );
+};
+
+export default ContentWrapper;
---
+++ src/components/ui/ContentWrapper/constants.ts
@@ -0,0 +1,2 @@
+export const DEFAULT_WIDTH = '1000px';
+export const DEFAULT_VARIANT = 'regular';
