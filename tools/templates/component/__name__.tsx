ad48927aac05a306435370c39e1334917b105839
Myron R Schippers Jr <dubtwee@gmail.com>
2025-07-22 14:50:24 -0400

    adding a dependency along with the creation of template files to generate components in a specific folder structure with all files needed

---
+++ tools/templates/component/__name__.tsx
@@ -0,0 +1,15 @@
+'use client';
+
+import { type FC } from 'react';
+
+import type { __name__Props } from './types';
+
+const __name__: FC<__name__Props> = () => {
+  return (
+    <div>
+      <h2>__name__(sentenceCase) component!</h2>
+    </div>
+  );
+};
+
+export default __name__;
